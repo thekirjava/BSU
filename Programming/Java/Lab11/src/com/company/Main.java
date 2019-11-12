93060b2dc0b09a913d91cf3c1afaa510c603917a
thekirjava <thekirjava@yandex.by>
2019-11-12 18:05:24 +0300

    Drawing panel started

--- Programming/Java/Lab11/src/com/company/Main.java
+++ Programming/Java/Lab11/src/com/company/Main.java
@@ -14,12 +14,12 @@ public class Main {
             this.setDefaultCloseOperation(EXIT_ON_CLOSE);
             Container cont = this.getContentPane();
             cont.setLayout(new BorderLayout());
-            drawSpace = new JPanel(null);
+            drawSpace = new DrawingPanel(this.getWidth(), this.getHeight());
             colorPanel = new JPanel(new FlowLayout());
             redButton = new JButton();
             greenButton = new JButton();
             blueButton = new JButton();
-            penColor = Color.WHITE;
+            penColor = Color.RED;
             this.repaint();
             redButton.setBackground(Color.RED);
 
@@ -45,13 +45,18 @@ public class Main {
 
                 @Override
                 public void mouseDragged(MouseEvent e) {
-
+                    Graphics g = drawSpace.getGraphics();
+                    Graphics gbuf = drawSpace.getBuffer().getGraphics();
+                    g.setColor(penColor);
+                    gbuf.setColor(penColor);
+                    g.fillRect(e.getX(), e.getY(), 1, 1);
+                    gbuf.fillRect(e.getX(), e.getY(), 1, 1);
                 }
             });
             cont.add(drawSpace, BorderLayout.CENTER);
             cont.add(colorPanel, BorderLayout.SOUTH);
         }
-        private JPanel drawSpace;
+        private DrawingPanel drawSpace;
         private JPanel colorPanel;
         private JButton redButton;
         private JButton greenButton;
