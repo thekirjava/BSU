044f474dc67bb452ededd4618b40ca2cbfc62094
thekirjava <thekirjava@yandex.by>
2019-11-12 17:46:39 +0300

    Window added

--- Programming/Java/Lab11/src/com/company/Main.java
+++ Programming/Java/Lab11/src/com/company/Main.java
@@ -1,8 +1,66 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class Main {
 
+    static class Window extends JFrame {
+        Window() {
+            this.setBounds(100, 100, 900, 500);
+            this.setTitle("Lab11");
+            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            Container cont = this.getContentPane();
+            cont.setLayout(new BorderLayout());
+            drawSpace = new JPanel(null);
+            colorPanel = new JPanel(new FlowLayout());
+            redButton = new JButton();
+            greenButton = new JButton();
+            blueButton = new JButton();
+            penColor = Color.WHITE;
+            this.repaint();
+            redButton.setBackground(Color.RED);
+
+            greenButton.setBackground(Color.GREEN);
+
+            blueButton.setBackground(Color.BLUE);
+            redButton.setPreferredSize(new Dimension(this.getWidth()/4, 30));
+            greenButton.setPreferredSize(new Dimension(this.getWidth()/4, 30));
+            blueButton.setPreferredSize(new Dimension(this.getWidth()/4, 30));
+            colorPanel.add(redButton);
+            colorPanel.add(greenButton);
+            colorPanel.add(blueButton);
+            drawSpace.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+                }
+
+                @Override
+                public void mouseDragged(MouseEvent e) {
+
+                }
+            });
+            cont.add(drawSpace, BorderLayout.CENTER);
+            cont.add(colorPanel, BorderLayout.SOUTH);
+        }
+        private JPanel drawSpace;
+        private JPanel colorPanel;
+        private JButton redButton;
+        private JButton greenButton;
+        private JButton blueButton;
+        private Color penColor;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Window w = new Window();
+        w.setVisible(true);
     }
 }
