67aed4f40316aea32ce749ab051c9f97910d24e4
thekirjava <thekirjava@yandex.by>
2020-03-07 19:26:17 +0300

    Algo removed

--- Numerical Analysis/sem4/Lab1/Newton.py
+++ Numerical Analysis/sem4/Lab1/Newton.py
@@ -31,6 +31,15 @@ def jacobian(v, size):
     return ans
 
 
+def f(x, b, size):
+    ans = np.array([float(0) for i in range(2 * size + 2)])
+    for i in range(len(ans)):
+        for j in range(0, len(x), 2):
+            ans[i] += x[j] * pow(x[j + 1], i)
+        ans[i] -= b[i]
+    return ans
+
+
 m = 1
 g = np.array(g_counter(m))
 Ax = np.array([random.uniform(-1, 1) for i in range(len(g))])
@@ -40,9 +49,9 @@ eps = pow(10, -10)
 k = 0
 while S > eps:
     omega = jacobian(Ax, m)
-    delta_x = np.linalg.solve(omega, g)
+    delta_x = np.linalg.solve(omega, f(Ax, g, m))
     S = np.linalg.norm(delta_x)
-    Ax += delta_x
+    Ax -= delta_x
     k += 1
     print(S)
 print(Ax)
