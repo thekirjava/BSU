59a3df7a4b868e09922941a027d1eae39f1b05b4
thekirjava <thekirjava@yandex.by>
2020-03-24 16:49:32 +0300

    Lab update

--- Programming/Java/sem4/Lab4/src/com/company/MainWindow.java
+++ Programming/Java/sem4/Lab4/src/com/company/MainWindow.java
@@ -8,7 +8,11 @@ import com.google.gson.stream.JsonReader;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -19,6 +23,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.AttributedString;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.List;
 import java.util.Timer;
@@ -79,14 +85,11 @@ public class MainWindow extends JFrame {
         JPanel task1 = new JPanel();
         JPanel task2 = new JPanel();
         JPanel task3 = new JPanel();
-        JPanel task4 = new JPanel();
         task1.setLayout(new BorderLayout());
         task2.setLayout(new BorderLayout());
         task3.setLayout(new BorderLayout());
-        task4.setLayout(new BorderLayout());
         DrawingPanel draw1 = new DrawingPanel(310, 310);
-        DrawingPanel draw2 = new DrawingPanel(510, 510);
-        DrawingPanel draw3 = new DrawingPanel(310, 310);
+        DrawingPanel draw2 = new DrawingPanel(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
         task1.add(draw1, BorderLayout.CENTER);
         task2.add(draw2, BorderLayout.CENTER);
 
@@ -136,10 +139,11 @@ public class MainWindow extends JFrame {
                 graphicsBuffer.setColor(penColor);
                 graphics.drawOval(0, 0, 300, 300);
                 graphicsBuffer.drawOval(0, 0, 300, 300);
+                tabbedPane.setEnabled(true);
             }
         };
-        timer.schedule(init, 700);
-        timer.schedule(clock, 750, 1000);
+        timer.schedule(init, 2000);
+        timer.schedule(clock, 2050, 1000);
 
 
         JSlider speed = new JSlider(1, 20, 10);
@@ -163,19 +167,22 @@ public class MainWindow extends JFrame {
         TimerTask runningPicture = new TimerTask() {
             @Override
             public void run() {
+                int w = MainWindow.this.getWidth();
+                int h = MainWindow.this.getHeight();
                 if ((draw2.getMovingPicture() != null) && (tabbedPane.getSelectedIndex() == 1)) {
                     Graphics graphics = draw2.getGraphics();
                     Graphics graphicsBuffer = draw2.getBuffer().getGraphics();
                     penColor = Color.WHITE;
                     graphics.setColor(penColor);
                     graphicsBuffer.setColor(penColor);
-                    graphics.fillRect(0, 0, 500, 500);
-                    graphicsBuffer.fillRect(0, 0, 500, 500);
+                    graphics.fillRect(0, 0, w, h);
+                    graphicsBuffer.fillRect(0, 0, w, h);
                     penColor = Color.BLACK;
                     graphics.setColor(penColor);
                     graphicsBuffer.setColor(penColor);
-                    double x = 150 + 150 * Math.cos(angle2);
-                    double y = 150 + 150 * Math.sin(angle2);
+                    double radius = Double.min(w / 2, h / 2) - 100;
+                    double x = w / 2 + radius * Math.cos(angle2);
+                    double y = h / 2 + radius * Math.sin(angle2);
 
                     graphics.drawImage(draw2.getMovingPicture(), (int) x, (int) y, null);
                     graphicsBuffer.drawImage(draw2.getMovingPicture(), (int) x, (int) y, null);
@@ -188,7 +195,7 @@ public class MainWindow extends JFrame {
                 }
             }
         };
-        timer.schedule(runningPicture, 750, 1000);
+        timer.schedule(runningPicture, 2050, 1000);
         task2.add(speed, BorderLayout.SOUTH);
         JPanel buttonPane = new JPanel();
         buttonPane.setLayout(new GridLayout(3, 1));
@@ -222,70 +229,17 @@ public class MainWindow extends JFrame {
 
             }
         });
-        DefaultListModel<LegendCell> listModel = new DefaultListModel<>();
-        JList<LegendCell> chartLegend = new JList<>(listModel);
-        chartLegend.setCellRenderer(new ChartLegendRenderer());
-        JButton jsonOpen2 = new JButton("Open");
-        jsonOpen2.addActionListener(e -> {
-            try {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setCurrentDirectory(new File("."));
-                fileChooser.setFileFilter(new FileNameExtensionFilter("JSON files", "json"));
-                if (fileChooser.showDialog(MainWindow.this, "Open") == JFileChooser.APPROVE_OPTION) {
-                    GsonBuilder builder = new GsonBuilder();
-                    Gson gson = builder.create();
-                    JsonReader reader = new JsonReader(new FileReader(fileChooser.getSelectedFile()));
-                    ArrayList<ParsedData> jsonParsed;
-                    jsonParsed = gson.fromJson(reader, new TypeToken<List<ParsedData>>() {
-                    }.getType());
-                    double allData = 0;
-                    for (ParsedData parsedData : jsonParsed) {
-                        allData += parsedData.value;
-                    }
-                    Graphics graphics = draw3.getGraphics();
-                    Graphics graphicsBuffer = draw3.getBuffer().getGraphics();
-                    penColor = Color.WHITE;
-                    graphics.setColor(penColor);
-                    graphicsBuffer.setColor(penColor);
-                    graphics.fillRect(0, 0, 300, 300);
-                    graphicsBuffer.fillRect(0, 0, 300, 300);
-                    penColor = Color.BLACK;
-                    graphics.setColor(penColor);
-                    graphicsBuffer.setColor(penColor);
-                    graphics.drawOval(0, 0, 300, 300);
-                    graphicsBuffer.drawOval(0, 0, 300, 300);
-                    graphics.drawLine(150, 150, 0, 150);
-                    graphicsBuffer.drawLine(150, 150, 0, 150);
-                    Random random = new Random(42);
-                    listModel.clear();
-                    for (ParsedData parsedData : jsonParsed) {
-                        penColor = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
-                        graphics.setColor(penColor);
-                        graphicsBuffer.setColor(penColor);
-                        graphics.fillArc(0, 0, 300, 300, (int) angle3, (int) (parsedData.value / allData * 360));
-                        angle3 += parsedData.value / allData * 360;
-                        listModel.addElement(new LegendCell(penColor, parsedData.name));
-                    }
-
-                }
-            } catch (FileNotFoundException ex) {
-                JOptionPane.showMessageDialog(MainWindow.this, "File doesn't exist", "Error", JOptionPane.ERROR_MESSAGE);
-            } catch (JsonSyntaxException ex) {
-                JOptionPane.showMessageDialog(MainWindow.this, "Incorrect JSON file", "Error", JOptionPane.ERROR_MESSAGE);
-
-            }
-        });
         JFreeChart chart = ChartFactory.createPieChart("Chart", pieDataset, true, true, false);
+        PieSectionLabelGenerator generator = new StandardPieSectionLabelGenerator("{0}: {1} ({2})", new DecimalFormat("0"), new DecimalFormat("0%"));
+        PiePlot plot = (PiePlot) chart.getPlot();
+        plot.setLabelGenerator(generator);
         ChartPanel pane = new ChartPanel(chart);
         task3.add(pane, BorderLayout.CENTER);
-        task4.add(draw3, BorderLayout.WEST);
-        task4.add(jsonOpen2, BorderLayout.SOUTH);
-        task4.add(chartLegend, BorderLayout.EAST);
         task3.add(jsonOpen1, BorderLayout.SOUTH);
         tabbedPane.add("Task 1", task1);
         tabbedPane.add("Task 2", task2);
-        tabbedPane.add("Task 3_1", task3);
-        tabbedPane.add("Task 3_2", task4);
+        tabbedPane.add("Task 3", task3);
+        tabbedPane.setEnabled(false);
         Container container = this.getContentPane();
         container.setLayout(new BorderLayout());
         container.add(tabbedPane, BorderLayout.CENTER);
