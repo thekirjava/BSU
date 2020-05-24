7f79b33e0576fb0e43a5c76ec3dffe8f2b9e2856
thekirjava <thekirjava@yandex.by>
2020-05-24 15:51:02 +0300

    lab4

--- Numerical Analysis/sem4/Lab4/integrate.py
+++ Numerical Analysis/sem4/Lab4/integrate.py
@@ -5,11 +5,15 @@ def function(x):
     return (math.e ** (-(x ** 2))) * math.sin(x ** 2)
 
 
+def weight(x):
+    return math.sqrt(1 - x ** 2);
+
+
 def quad_formula(a, b, n):
     ans = function(a)
     h = (b - a) / (n - 1)
     for i in range(1, n):
-        ans += 2 * function(a + i * h)
+        ans += 2 * function(a + i * h) * weight(a + i * h)
     ans += function(b)
     ans *= h / 2
     return ans
@@ -29,11 +33,11 @@ while TETA * math.fabs(cur - double) >= EPSILON:
     cur = double
     double = quad_formula(left, right, 2 * n0)
     counts += 1
-A = [0.0575094, 0.196349, 0.33519, 0.392698, 0.33519, 0.19635, 0.0575094]
-nast_roots = [-0.92388, -0.707107, -0.382683, 0, 0.382683, 0.707107, 0.92388]
-nast = 0.0
+A = [0.0575092, 0.19635, 0.335189, 0.392699, 0.335189, 0.19635, 0.0575091]
+gauss_roots = [-0.92388, -0.707107, -0.382683, 0, 0.382683, 0.707107, 0.92388]
+gauss = 0.0
 for i in range(7):
-    nast += A[i] * function(nast_roots[i])
-print(double)
-print(nast)
-print(counts)
+    gauss += A[i] * function(gauss_roots[i])
+print("Integrate with trapeze =", double, sep=" ")
+print("Iterations =", counts, sep=" ")
+print("Gauss formula =", gauss, sep=" ")
